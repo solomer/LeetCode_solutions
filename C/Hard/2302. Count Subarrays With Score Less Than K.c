0a52c94a98da77fa442b76f77add720ffbac9e43
promer52554 <53744726+promer52554@users.noreply.github.com>
2025-05-01 23:24:45 +0300

    first commmit - added 2302 - hard

---
+++ C/Hard/2302. Count Subarrays With Score Less Than K.c
@@ -0,0 +1,26 @@
+//Problem: 2302. Count Subarrays With Score Less Than K
+//Difficulty: Hard
+//Link : https://leetcode.com/problems/count-subarrays-with-score-less-than-k/description/?envType=daily-question&envId=2025-04-28
+//Author: solomer
+
+long long countSubarrays(int* nums, int numsSize, long long k) {
+  int right = 0,left=0;
+  long long sum = 0;
+  long counter = 0;
+  while(right < numsSize){
+      sum += nums[right];
+      right++;
+      if (sum * (right - left) < k)
+          counter += right - left;
+      else
+          while (1) {
+              sum -= nums[left];
+              left++;
+              if (sum * (right - left) < k) {
+                  counter += right - left;
+                  break;
+              }
+          }
+  }
+  return counter;
+}
