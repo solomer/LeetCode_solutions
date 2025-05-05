953a2cd547384a5311f8649b510e02c49ac45420
promer52554 <53744726+promer52554@users.noreply.github.com>
2025-05-05 23:34:22 +0300

    added 135. candy -hard

---
+++ C/Hard/135. Candy.c
@@ -0,0 +1,29 @@
+//Problem: 135. Candy
+//Difficulty: Hard
+//Link : https://leetcode.com/problems/candy/description/
+//Author: solomer
+
+// Note: int this problem, I had to learn how to do window sliding because my early brute force method didnt work.
+
+int candy(int* ratings, int ratingsSize) {
+  int did = 1;
+  int sum = 0;
+  int candies[ratingsSize];
+  for (int i = 0; i < ratingsSize; i++) {
+      candies[i] = 1;
+  }
+  for (int i = ratingsSize - 1; i > 0; i--) {//from right to left
+      if (ratings[i - 1] > ratings[i] && !(candies[i - 1] > candies[i])) {
+          candies[i - 1] = candies[i] + 1;
+      }
+  }
+  for (int i = 0; i < ratingsSize - 1; i++) {//from left to right
+      if (ratings[i + 1] > ratings[i] && !(candies[i + 1] > candies[i])) {
+          candies[i + 1] = candies[i] + 1;
+      }
+  }
+  for (int i = 0; i < ratingsSize; i++) {
+      sum += candies[i]; //summation
+  }
+  return sum;
+}
